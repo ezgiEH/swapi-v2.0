8faea35c808b3a332c9ced9e38985855a62df80f
Ezgi Hocaoğlu <ezgihocaoglu@hotmail.com>
2022-12-30 15:58:58 +0300

    contact

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -2,47 +2,53 @@ import React from 'react'
 import styles from './Contact.module.css'
 import github from './github.svg'
 import linkedin from './linkedin.svg'
-import website from  './code-solid.svg'
- 
+import website from './code-solid.svg'
+
 
 function Contact() {
   return (
     <div className={styles.contact}>
-    <div className={styles.container}>
-      <header>If you have any question/feedback, please do not hesitate to reach out to <a href='#'>Me!.</a></header>
-      <div className={styles.box}>
-        <div className={styles.item}>
-        <div className={styles.rectangle}></div>
-          <figure>
-            <img src={github} alt='ezgiEh github'></img>
-            <figcaption>
-              <header>GitHub</header>
-              <p>Detail</p>
-            </figcaption>
-          </figure>
-        </div>
-        <div className={styles.item}>
-          <div className={styles.rectangle}></div>
-        <figure>
-            <img src={linkedin} alt='ezgiEh github'></img>
-            <figcaption>
-              <header>Linkedin</header>
-              <p>Detail</p>
-            </figcaption>
-          </figure>
-        </div>
-        <div className={styles.item}>
-        <div className={styles.rectangle}></div>
-        <figure>
-            <img src={website}  alt='ezgi hoceoglu'></img>
-            <figcaption>
-              <header>Web Site</header>
-              <p>Detail</p>
-            </figcaption>
-          </figure>
+      <div className={styles.container}>
+        <header>If you have any question/feedback, please do not hesitate to reach out to <a href='#'>Me!.</a></header>
+        <div className={styles.box}>
+          <div className={styles.item}>
+            <div className={styles.rectangle}></div>
+            <a href='https://github.com/ezgiEH' target="_blank">
+              <figure>
+                <img src={github} alt='ezgiEh github'></img>
+                <figcaption>
+                  <header>GitHub</header>
+                  <p>Detail</p>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
+          <div className={styles.item}>
+            <div className={styles.rectangle}></div>
+            <a href='https://www.linkedin.com/in/ezgihocaoglu/' target="_blank">
+              <figure>
+                <img src={linkedin} alt='ezgiEh github'></img>
+                <figcaption>
+                  <header>Linkedin</header>
+                  <p>Detail</p>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
+          <div className={styles.item}>
+            <div className={styles.rectangle}></div>
+            <a href='http://hello.ezgihocaoglu.co/' target="_blank">
+              <figure>
+                <img src={website} alt='ezgi hoceoglu'></img>
+                <figcaption>
+                  <header>Web Site</header>
+                  <p>Detail</p>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   )
 }
--- src/Pages/StarshipDetail/StarshipDetail.js
+++ src/Pages/StarshipDetail/StarshipDetail.js
@@ -1,4 +1,4 @@
-import { useEffect} from 'react'
+import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { fetchStarship } from '../../Redux/Starwars/Sevices';
@@ -14,49 +14,49 @@ function StarshipDetail() {
 
   useEffect(() => {
     dispatch(fetchStarship({ id }))
-}, [dispatch, id])
+  }, [dispatch, id])
 
   console.log(starship)
   if (loading === 'loading') {
     return (
-        <div className={styles.spinnerContainer}>
-            <div className={styles.spinner}></div>
-        </div>
+      <div className={styles.spinnerContainer}>
+        <div className={styles.spinner}></div>
+      </div>
     )
-}
-else {
+  }
+  else {
     return (
-        <div className={styles.container}>
-            <div className={styles.box}>
-              <div className={styles.leftSection}>
-              <div className={styles.shipName}><p>{starship.name}</p></div>
-               <div className={styles.floating} ><img src={`/assets/ships/${id}.png`} alt='starship'></img></div>
-              </div>
-              <div className={styles.rectangle}></div>
-              <div className={styles.rigthSection}>
-                <div className={styles.card}>
-                  <div className={styles.badget}>{starship.model}</div>
-                  <div className={styles.info}>
-                    <ul>
-                      <li><p>Manufacturer: <b>{starship.manufacturer}</b></p></li>
-                      <li><p>Cost in Credits: <b>{starship.cost_in_credits}</b></p></li>
-                      <li><p>Length: <b>{starship.length}</b></p></li>
-                      <li><p>Max Atmosphering Speed: <b>{starship.max_atmosphering_speed}</b></p></li>
-                      <li><p>Crew: <b>{starship.crew}</b></p></li>
-                      <li><p>Passengers: <b>{starship.passengers}</b></p></li>
-                      <li><p>Cargo Capacity: <b>{starship.cargo_capacity}</b></p></li>
-                      <li><p>Starship Class: <b>{starship.starship_class}</b></p></li>
-                      <li><p>Hyperdrive Rating: <b>{starship.hyperdrive_rating}</b></p></li>
-                      <li><p>MGLT: <b>{starship.MGLT}</b></p></li>
-                      <li><p>Consumables: <b>{starship.consumables}</b></p></li>
-                    </ul>
-                  </div>
-                </div>
+      <div className={styles.container}>
+        <div className={styles.box}>
+          <div className={styles.leftSection}>
+            <div className={styles.shipName}><p>{starship.name}</p></div>
+            <div className={styles.floating} ><img src={`/assets/ships/${id}.png`} alt='starship'></img></div>
+          </div>
+          <div className={styles.rectangle}></div>
+          <div className={styles.rigthSection}>
+            <div className={styles.card}>
+              <div className={styles.badget}>{starship.model}</div>
+              <div className={styles.info}>
+                <ul>
+                  <li><p>Manufacturer: <b>{starship.manufacturer}</b></p></li>
+                  <li><p>Cost in Credits: <b>{starship.cost_in_credits}</b></p></li>
+                  <li><p>Length: <b>{starship.length}</b></p></li>
+                  <li><p>Max Atmosphering Speed: <b>{starship.max_atmosphering_speed}</b></p></li>
+                  <li><p>Crew: <b>{starship.crew}</b></p></li>
+                  <li><p>Passengers: <b>{starship.passengers}</b></p></li>
+                  <li><p>Cargo Capacity: <b>{starship.cargo_capacity}</b></p></li>
+                  <li><p>Starship Class: <b>{starship.starship_class}</b></p></li>
+                  <li><p>Hyperdrive Rating: <b>{starship.hyperdrive_rating}</b></p></li>
+                  <li><p>MGLT: <b>{starship.MGLT}</b></p></li>
+                  <li><p>Consumables: <b>{starship.consumables}</b></p></li>
+                </ul>
               </div>
             </div>
+          </div>
         </div>
+      </div>
     )
-}
+  }
 }
 
 
